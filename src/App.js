22620e6d2e67363da67eb40bc71d504f852f7491
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-08-17 21:34:50 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+const App = () => {
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+  const [hemisphere, setHemisphere] = useState("");
+  const [month, setMonth] = useState(""); 
+
+  // Date object
+
+
+  // console.log("hello") // useEffect
+
+  function findLocation() {
+      // 
+      if(navigator.geolocation){
+          navigator.geolocation.getCurrentPosition(
+              (position) =>{
+                //  console.log(typeof(position.coords.latitude));
+                  setLat(position.coords.latitude);
+                  // console.log(lat)
+                  setLong(position.coords.longitude);
+
+                  updateHemisphere(position.coords.latitude)
+              }
+          );
+      } 
+  }
+
+  function updateHemisphere(lat){
+    if(lat > 0){
+        setHemisphere("Northern Hemisphere")
+    }else if(lat < 0){
+        setHemisphere("Southern Hemisphere")
+    }else{
+        setHemisphere("Equator")
+    }
+  }
+
+
+
+  return(
+    <div>
+          <button onClick={findLocation}>Find My Location</button>
+          <h1>Latitude: {lat}</h1>
+          <h1>Longitude: {long}</h1>
+          {/* {
+            lat &&  updateHemisphere()
+          } */}
+          <h1>Hemisphere: {hemisphere}</h1>
     </div>
-  );
+      
+  )
 }
 
 export default App;
+
+
+
+
+// Northern Hemisphere: 
+// March to Spetember => Summer  => souther => winter
+// Jan, Feb, Oct, Nov , dec=> Winter => souther => summer
